97601dc5f8477ebcf18618d7041581ffa79d803e
minu <gkqkehs10@naver.com>
2023-02-25 14:03:41 +0900

    feat-initailze

---
+++ index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { createServer } from "http";
+
+import cors from "cors";
+import hpp from "hpp";
+import helmet from "helmet";
+
+const app = express();
+const httpServer = createServer(app);
+//swagger
+// const swaggerUi = require("swagger-ui-express");
+// const YAML = require("yamljs");
+// const swaggerSpec = YAML.load(path.join(__dirname, "swagger.yaml"));
+
+//서버 가동
+dotenv.config();
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(hpp());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// swagger
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerSpec, { explorer: true }) //검색 허용가능
+// );
+
+app.use((error: unknown, req, res, next) => {
+  res.json({ message: error.message });
+});
+
+httpServer.listen(80);
