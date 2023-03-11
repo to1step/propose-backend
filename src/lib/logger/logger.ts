b2e1a17eb15be63dddf9663b50e7bbc47ab7640c
minwoo <gkqkehs0321@gmail.com>
2023-03-11 15:41:52 +0900

    WIP

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -9,48 +9,36 @@ const notLocal =
 
 // log출력 형식 정의
 const logFormat = printf((info) => {
-	return `${info.timestamp} ${info.level}: ${info.message}`; // 날짜 시간 로그레벨: 메세지
+	return `[${info.timestamp}] | [${info.level}] | ${info.message}`; // 날짜 시간 로그레벨: 메세지
 });
 
-const infoLog = new WinstonDaily({
-	level: 'info',
-	datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
-	dirname: `${process.cwd()}/logs/info`, // 파일 경로
-	filename: `%DATE%.info.log`, // 파일 이름 형식 2020-05-28.info.log
-	maxFiles: 30, // 최근 30일치 로그 파일을 남김
-	handleExceptions: true,
-});
+const logFile = (level: string, directory: string) => {
+	return new WinstonDaily({
+		level: `${level}`,
+		datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
+		dirname: `${process.cwd()}/logs/${directory}`, // 파일 경로
+		filename: `%DATE%.${directory}.log`, // 파일 이름 형식 2020-05-28.info.log
+		maxFiles: 30, // 최근 30일치 로그 파일을 남김
+		handleExceptions: true,
+	});
+};
 
-const errorLog = new WinstonDaily({
-	level: 'error',
-	datePattern: 'YYYY-MM-DD',
-	dirname: `${process.cwd()}/logs/error`,
-	filename: `%DATE%.error.log`, // 파일 이름 형식 2020-05-28.error.log
-	maxFiles: 30,
-	handleExceptions: true,
-});
-
-const exceptionLog = new WinstonDaily({
-	level: 'error',
-	datePattern: 'YYYY-MM-DD',
-	dirname: `${process.cwd()}/logs/exception`,
-	filename: `%DATE%.exception.log`, // 파일 이름 형식 2020-05-28.error.log
-	maxFiles: 30,
-	handleExceptions: true,
-});
-
-const terminalLog = new transports.Console({
+const logTerminal = new transports.Console({
 	format: format.combine(
 		format.colorize(), // 색깔 넣어서 출력
 		timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
 		logFormat
 	),
+	handleExceptions: true,
 });
 
 const logger = createLogger({
 	format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
-	transports: notLocal ? [infoLog, errorLog] : terminalLog,
-	exceptionHandlers: notLocal ? [exceptionLog] : terminalLog,
+	exitOnError: false,
+	transports: notLocal
+		? [logFile('info', 'info'), logFile('error', 'error')]
+		: logTerminal,
+	exceptionHandlers: notLocal ? [logFile('error', 'exception')] : logTerminal,
 });
 
 export { logger };
