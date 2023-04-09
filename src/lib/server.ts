56ab66acfcfb10fffbc4eda51e13872194f596e4
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-04-09 14:46:57 +0900

    #21 Fix-winston logger method

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,7 +1,7 @@
 import WintonLogger from './logger/logger';
 import app from './app';
 
-const logger = WintonLogger.getInstance().getLogger();
+const logger = WintonLogger.getInstance();
 
 const server = app.listen(app.get('port'), () => {
 	logger.info(`🛡️  Server listening on port: ${app.get('port')}🛡️`);
