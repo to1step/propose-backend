c915e8c75ad44158a2aba17ae90caf27110bc9f8
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-04-13 15:33:47 +0900

    31 Swagger initial setting

---
+++ src/utilies/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+			version: '1.0.0',
+			title: 'to1step api docs',
+			description: 'to1step api 문서랍니다',
+		},
+		servers: [
+			{
+				url: 'http://localhost:4000',
+			},
+		],
+	},
+	apis: ['src/lib/routes/*.ts'], // 절대경로로 작성해주어야 작동
+};
+const swaggerOption = swaggerJsdoc(options);
+
+export default swaggerOption;
