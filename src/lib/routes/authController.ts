67629b2980bac024b5576708e8406cbbe7311e77
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-05-11 22:28:03 +0900

    #46 Swagger Docs to one file

--- src/lib/routes/authController.ts
+++ src/lib/routes/authController.ts
@@ -10,38 +10,6 @@ import NicknameValidationDto from '../types/requestTypes/nicknameValidation.dto'
 const router = express.Router();
 const authService = AuthService.getInstance();
 
-/**
- * @swagger
- * /auth/local/sign-in:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 로컬 로그인
- *     description: 로컬 로그인
- *     requestBody:
- *       description: LocalSignInDto
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/LocalSignInDto"
- *     responses:
- *       '200':
- *         description: 로그인 성공
- *         header:
- *           Set-Cookie:
- *             description: 쿠기 값
- *             schema:
- *               type: string
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 로그인 성공
- *                   example: true
- */
 router.post('/auth/local/sign-in', async (req, res, next) => {
 	try {
 		const localSignInDto = new LocalSignInDto(req.body);
@@ -62,34 +30,6 @@ router.post('/auth/local/sign-in', async (req, res, next) => {
 });
 
 //#region 로컬 회원가입
-
-/**
- * @swagger
- * /auth/local/email-validation:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 이메일 중복체크
- *     description: 이메일 다른 유저가 사용중인지 확인
- *     requestBody:
- *       description: EmailValidationDto
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/EmailValidationDto"
- *     responses:
- *       '200':
- *         description: 이메일 사용가능 여부
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 사용중이면 true / 사용중이 아니면 false 반환
- *                   example: true
- */
 router.post('/auth/local/email-validation', async (req, res, next) => {
 	try {
 		const emailValidationDto = new EmailValidationDto(req.body);
@@ -106,35 +46,6 @@ router.post('/auth/local/email-validation', async (req, res, next) => {
 	}
 });
 
-/**
- * @swagger
- * /auth/local/nickname-validation:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 닉네임 중복체크
- *     description: 닉네임 다른 유저가 사용중인지 확인
- *     produces:
- *     - application/json
- *     requestBody:
- *       description: NicknameValidationDto
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/NicknameValidationDto"
- *     responses:
- *       '200':
- *         description: 닉네임 사용가능 여부
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 사용중이면 true / 사용중이 아니면 false 반환
- *                   example: true
- */
 router.post('/auth/local/nickname-validation', async (req, res, next) => {
 	try {
 		const nicknameValidationDto = new NicknameValidationDto(req.body);
@@ -151,33 +62,6 @@ router.post('/auth/local/nickname-validation', async (req, res, next) => {
 	}
 });
 
-/**
- * @swagger
- * /auth/local/email-code:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 인증메일 전송
- *     description: 유저정보 redis에 저장, 인증메일 전송
- *     requestBody:
- *       description: UserDataDto
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/UserDataDto"
- *     responses:
- *       '200':
- *         description: 인증 메일 전송 여부
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 이메일 전송 성공
- *                   example: true
- */
 router.post('/auth/local/email-code', async (req, res, next) => {
 	try {
 		const userIp = req.ip;
@@ -194,33 +78,6 @@ router.post('/auth/local/email-code', async (req, res, next) => {
 	}
 });
 
-/**
- * @swagger
- * /auth/local/email-verification:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 이메일 인증 및 유저 회원가입
- *     description: redis에 저장된 인증번호와 비교 후 일치 시 유저 회원가입
- *     headers:
- *       description: EmailVerificationDto
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/EmailVerificationDto"
- *     responses:
- *       '200':
- *         description: 회원가입 성공 즉시 로그인
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 회원가입 성공
- *                   example: true
- */
 router.post('/auth/local/email-verification', async (req, res, next) => {
 	try {
 		const userIp = req.ip;
@@ -244,30 +101,6 @@ router.post('/auth/local/email-verification', async (req, res, next) => {
 });
 //#endregion
 
-/**
- * @swagger
- * /auth/reissue:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 토큰 재발급
- *     description: refreshToken을 header에 담아 요청 토큰이 유효하다면 accessToken 재발급
- *     parameters:
- *     - in: header
- *       name: refreshToken
- *     responses:
- *       '200':
- *         description: accessToken 갱신
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 토큰 재발급 성공
- *                   example: true
- */
 router.post('/auth/token', (req, res, next) => {
 	const refreshToken = req.header('refreshToken');
 
@@ -280,30 +113,6 @@ router.post('/auth/token', (req, res, next) => {
 	res.cookie('accessToken', accessToken).json({ data: true });
 });
 
-/**
- * @swagger
- * /auth/sign-out:
- *   post:
- *     tags:
- *       - AuthController
- *     summary: 로그아웃
- *     description: 토큰이 유효한 유저를 로그아웃시킴
- *     parameters:
- *     - in: header
- *       name: refreshToken
- *     responses:
- *       '200':
- *         description: 로그아웃 성공
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: boolean
- *                   description: 로그아웃 성공
- *                   example: true
- */
 router.post('/auth/sign-out', async (req, res, next) => {
 	try {
 		const refreshToken = req.header('refreshToken');
