17e4d95b94347dd41c436a4f7bd8407a19ed1206
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-05-18 12:02:34 +0900

    #50 Apply store review api

--- src/lib/routes/storeController.ts
+++ src/lib/routes/storeController.ts
@@ -6,6 +6,8 @@ import CreateStoreDto from '../types/requestTypes/createStore.dto';
 import UpdateStoreDto from '../types/requestTypes/updateStore.dto';
 import LikeStoreDto from '../types/requestTypes/likeStore.dto';
 import UnlikeStoreDto from '../types/requestTypes/unlikeStore.dto';
+import CreateStoreReviewDto from '../types/requestTypes/createStoreReview.dto';
+import UpdateStoreReviewDto from '../types/requestTypes/updateStoreReview.dto';
 
 const router = express.Router();
 const storeService = StoreService.getInstance();
@@ -130,5 +132,71 @@ router.post('/store/unlike', checkHeaderToken, async (req, res, next) => {
 		next(error);
 	}
 });
+
+router.post('/store/review', checkHeaderToken, async (req, res, next) => {
+	try {
+		if (!req.userUUID) {
+			throw new Error('invalid user');
+		}
+
+		const createStoreReviewDto = new CreateStoreReviewDto(req.body);
+
+		await validateOrReject(createStoreReviewDto);
+
+		await storeService.createStoreReview(
+			createStoreReviewDto.toServiceModel(),
+			req.userUUID
+		);
+
+		res.json({ data: true });
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.patch('/store/review', checkHeaderToken, async (req, res, next) => {
+	try {
+		if (!req.userUUID) {
+			throw new Error('invalid user');
+		}
+
+		const updateStoreReviewDto = new UpdateStoreReviewDto(req.body);
+
+		await validateOrReject(updateStoreReviewDto);
+
+		await storeService.updateStoreReview(
+			updateStoreReviewDto.toServiceModel(),
+			req.userUUID
+		);
+
+		res.json({ data: true });
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.delete(
+	'/store/review/:storeReviewUUID',
+	checkHeaderToken,
+	async (req, res, next) => {
+		try {
+			if (!req.userUUID) {
+				throw new Error('invalid user');
+			}
+
+			const { storeReviewUUID } = req.params;
+
+			if (!storeReviewUUID) {
+				throw new Error('no storeId params');
+			}
+
+			await storeService.removeStoreReview(storeReviewUUID, req.userUUID);
+
+			res.json({ data: true });
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 export default router;
